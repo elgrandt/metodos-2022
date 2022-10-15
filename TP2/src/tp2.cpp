01e0de1573412c1da7de1324aa98817de45bbc5f
dtasat <dylantasat11@gmail.com>
2022-10-15 14:58:35 -0300

    Cambios hechos en la clase del 14-10. Ahora funciona el código c++. Terminada sección 2 del código python.

--- TP2/src/tp2.cpp
+++ TP2/src/tp2.cpp
@@ -16,11 +16,6 @@ struct Resultado {
 
 bool converged(SparseMatrix& current, SparseMatrix& previous, double tolerancia) {
     return fabs(fabs((previous.transpose() * current).at(0, 0)) - 1) < tolerancia;
-    // for (int i = 0; i < current.getHeight(); i++) {
-    //     if (fabs(current.at(i, 0) - previous.at(i, 0)) > tolerancia)
-    //         return false;
-    // }
-    // return true;
 }
 
 Resultado metodo_potencia(SparseMatrix& matriz, int iteraciones, double tolerancia) {
@@ -30,26 +25,22 @@ Resultado metodo_potencia(SparseMatrix& matriz, int iteraciones, double toleranc
         x0.blind_set(i, 0, 1 + (rand() % 9)); // Lleno x0 de numeros random entre 1 y 10
     }
     x0 = x0 * (1.0 / x0.norm());
-    cout << "X0 = " << x0.transpose();
     // Iteramos
     SparseMatrix previous = SparseMatrix(0, 0);
     for (int iter = 0; iter < iteraciones; iter++) {
-        cout << "Iteracion " << iter << endl;
         previous = SparseMatrix(x0);
         x0 = matriz * x0;
-        cout << "Resultado de matriz * x0: " << x0.transpose();
         x0 = x0 * (1.0 / x0.norm());
-        cout << "Resultado de normalizar: " << x0.transpose();
         // Comparo x0 con previous
         if (converged(x0, previous, tolerancia))
             break;
     }
     // Si no convergio probamos de nuevo
     if (!converged(x0, previous, tolerancia)) {
-        return metodo_potencia(matriz, iteraciones, tolerancia);
+        throw runtime_error("No converge en la cantidad de iteraciones requeridas.");
     }
     // Calculamos el autovalor
-    double autovalor = ((x0.transpose() * matriz * x0).at(0, 0) * (1/(x0.transpose() * x0).at(0, 0)));
+    double autovalor = ((x0.transpose() * matriz * x0).at(0, 0) / (x0.transpose() * x0).at(0, 0));
     // Generamos el resultado
     return Resultado(autovalor, x0);
 }
@@ -62,8 +53,7 @@ int main(int argc, char** argv) {
     int numero_iteraciones = 1;
     double tolerancia = 0.1;
     if (argc != 4) {
-        cerr << "Ingreso incorrecto de parametros, uso correcto es ./tp2 archivo #iteraciones tolerancia" << endl;
-        return -1;
+        throw runtime_error("Ingreso incorrecto de parametros, uso correcto es ./tp2 archivo #iteraciones tolerancia");
     } else {
         input_file = argv[1];
         numero_iteraciones = stoi(argv[2]);
@@ -73,8 +63,7 @@ int main(int argc, char** argv) {
     // Leemos el input
     fstream fin (input_file);
     if (fin.fail() || !fin.is_open()) {
-        cerr << "Error al abrir el archivo: " << input_file << endl;
-        return -1;
+        throw runtime_error("Error al abrir el archivo");
     }
 
     vector<vector<double>> vector_matriz = vector<vector<double>>();
@@ -95,8 +84,7 @@ int main(int argc, char** argv) {
     }
     
     if (vector_matriz.size() == 0 || vector_matriz[0].size() == 0) {
-        cerr << "Se leyo una matriz vacia" << endl;
-        return -1;
+        throw runtime_error("Se leyo una matriz vacia");
     }
     SparseMatrix matriz(vector_matriz[0].size(), vector_matriz.size());
     for (int row = 0; row < vector_matriz.size(); row++) {
@@ -112,17 +100,11 @@ int main(int argc, char** argv) {
     vector<Resultado> resultados;
 
     for (int i = 0; i < matriz.getHeight(); i++) {
-        cout << "<<<<<<<<<<<<<< Comenzando calculo del autovalor " << i+1 << " con la matriz:" << endl;
-        cout << matriz_actual;
         Resultado resultado = metodo_potencia(matriz_actual, numero_iteraciones, tolerancia);
         resultados.push_back(resultado);
-        matriz_actual = matriz_actual - ( resultado.autovector * resultado.autovector.transpose() );
         cout << "Autovalor " << i+1 << ": " << resultado.autovalor << endl;
         cout << "Autovector " << i+1 << ": " << resultado.autovector.transpose();
-        cout << "A*v = " << endl;
-        cout << (matriz * resultado.autovector).transpose();
-        cout  << "Autovalor * v = " << endl;
-        cout << (resultado.autovector * resultado.autovalor).transpose();
+        matriz_actual = matriz_actual - ( resultado.autovector * resultado.autovector.transpose() ) * resultado.autovalor;
     }
     
     string input_file_name = input_file.substr(input_file.find_last_of("/\\") + 1);
